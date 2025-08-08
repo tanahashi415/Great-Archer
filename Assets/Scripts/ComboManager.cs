aa8e0e9a89bc64c140a1e0b82d6bfc7f5a1629d0
Tanahashi <yoshimasa_notepc@outlook.jp>
2025-08-09 07:05:37 +0900

    add: Combo system

---
+++ Assets/Scripts/ComboManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class ComboManager : MonoBehaviour
+{
+    private static bool isCombo;    // コンボしているか
+    public static int combo;        // コンボ数
+
+    private Coroutine coroutine;            // コールチン格納用
+    private TextMeshProUGUI comboText;      // テキスト
+    private RectTransform rectTransform;    // 位置
+    private Vector2 initialPos;             // 初期位置
+
+    public GameObject text; // テキストオブジェクトのインスタンス
+    public float timeLimit; // コンボ受付時間
+
+    void Start()
+    {
+        comboText = text.GetComponent<TextMeshProUGUI>();
+        rectTransform = text.GetComponent<RectTransform>();
+        initialPos = rectTransform.position;
+        comboText.enabled = false;
+        combo = 0;
+    }
+
+    void Update()
+    {
+        if (isCombo)
+        {
+            // 前のコールチンを停止
+            if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+                rectTransform.position = initialPos;
+            }
+            // 新たにコールチンを開始
+            coroutine = StartCoroutine(DisplayCombo());
+            isCombo = false;
+        }
+        
+    }
+
+    public static void Combo()
+    {
+        combo++;
+        isCombo = true;
+    }
+
+    IEnumerator DisplayCombo()
+    {
+        // コンボ表示
+        comboText.enabled = true;
+        comboText.text = combo.ToString() + " Combo!!";
+
+        // 振動させる
+        float time = 0.025f;
+        WaitForSeconds wait = new WaitForSeconds(time);
+        rectTransform.position = new Vector2(rectTransform.position.x, rectTransform.position.y + 20.0f);
+        yield return wait;
+        rectTransform.position = new Vector2(rectTransform.position.x, rectTransform.position.y - 40.0f);
+        yield return wait;
+        rectTransform.position = new Vector2(rectTransform.position.x, rectTransform.position.y + 30.0f);
+        yield return wait;
+        rectTransform.position = new Vector2(rectTransform.position.x, rectTransform.position.y - 20.0f);
+        yield return wait;
+        rectTransform.position = new Vector2(rectTransform.position.x, rectTransform.position.y + 10.0f);
+        if (timeLimit > 0.1f)
+        {
+            yield return new WaitForSeconds(timeLimit - 0.1f);
+        }
+
+        // コンボ非表示
+        comboText.enabled = false;
+        combo = 0;
+    }
+}
